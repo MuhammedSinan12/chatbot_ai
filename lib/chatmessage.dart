47a9495ad8e77a20ab3d445aba85b76907256e42
MuhammedSinan12 <sinusinu1222@.com>
2023-02-01 21:01:16 +0530

    error

--- lib/chatmessage.dart
+++ lib/chatmessage.dart
@@ -2,10 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class ChatMessage extends StatelessWidget {
-  const ChatMessage({super.key, required this.text, required this.sender});
+  const ChatMessage(
+      {super.key,
+      required this.text,
+      required this.sender,
+      this.isImage = false});
 
   final String text;
   final String sender;
+  final bool isImage;
 
   @override
   Widget build(BuildContext context) {
@@ -17,12 +22,25 @@ class ChatMessage extends StatelessWidget {
             .subtitle1(context)
             .make()
             .box
-            .color(sender == "User" ? Vx.red200 : Vx.green200)
+            .color(sender == "user" ? Vx.red200 : Vx.green200)
             .p16
             .rounded
             .alignCenter
             .makeCentered(),
-        Expanded(child: text.trim().text.bodyText1(context).make().px8())
+        Expanded(
+          child: isImage
+              ? AspectRatio(
+                  aspectRatio: 16 / 9,
+                  child: Image.network(
+                    text,
+                    loadingBuilder: (context, child, loadingProgress) =>
+                        loadingProgress == null
+                            ? child
+                            : const CircularProgressIndicator.adaptive(),
+                  ),
+                )
+              : text.trim().text.bodyText1(context).make().px8(),
+        ),
       ],
     ).py8();
   }
